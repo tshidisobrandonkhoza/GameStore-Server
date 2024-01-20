8c135b0ef5c8e7e63e1b6585b0a6043588f27b0c
brandonkhoza <8brandonkhoza@gmail.com>
2024-01-20 19:52:45 +0200

    Added GET METHODS and a list of games

---
+++ Models/Game.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Server.Models
+{
+    public class Game 
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required String Name { get; set; }
+       
+        [Required] 
+        [StringLength(20)]
+        public String Genre { get; set; } = String.Empty;
+       
+        [Range(1, 20)]
+        public Decimal Price { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,48 @@
+using GameStore.Server.Models;
+
+//Just a list of games
+List<Game> games = new(){
+    new Game(){
+     Id= 1,
+     Name = "Street Fighter",
+     Genre = "Fighting",
+     Price = 19.99M,
+     ReleasedDate = new DateTime(2000,2,1)
+    },  new Game(){
+     Id= 2,
+     Name = "Final Fantasy",
+     Genre = "Playing",
+     Price = 29.99M,
+     ReleasedDate = new DateTime(2007,7,16)
+    },  new Game(){
+     Id= 3,
+     Name = "Fifa",
+     Genre = "Soccer",
+     Price = 49.99M,
+     ReleasedDate = new DateTime(2020,4,12)
+    }
+   };
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+//GET METHOD | Index
+app.MapGet("/", () => "Hello, we have succesfully reached the index, Happy!!");
 
-app.MapGet("/", () => "Hello World!");
+//GET METHOD | Games
+app.MapGet("/games", () => games);
+
+//GET METHOD | A single Game
+app.MapGet("/games/{id}", (int id) =>
+{
+   //declare a variable with a Game DataType
+   Game game = games.Find(game => game.Id == id);
+
+   if (game is null)
+   {
+      return Results.NotFound();
+   }
+   return Results.Ok(game);
+});
 
 app.Run();
